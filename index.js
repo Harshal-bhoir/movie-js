a7c2cef141c668713dba2c7004148e209cb8327a
Harshal Bhoir <harshalat333@gmail.com>
2022-07-12 22:42:00 +0530

    http changed to https

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const autoCompleteConfig = {
   },
 
   async fetchData(searchTerm) {
-    const response = await axios.get("http://www.omdbapi.com/", {
+    const response = await axios.get("https://www.omdbapi.com/", {
       params: {
         apikey: "96f25329",
         s: searchTerm,
@@ -44,7 +44,7 @@ let leftMovie;
 let rightMovie;
 
 const onMovieSelect = async (movie, summaryElement, side) => {
-  const response = await axios.get("http://www.omdbapi.com/", {
+  const response = await axios.get("https://www.omdbapi.com/", {
     params: {
       apikey: "96f25329",
       i: movie.imdbID,
